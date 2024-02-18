c858860a9e249bda880974ac07ac5cf51e4d14ed
AhmedAlsheikh0234 <154998275+AhmedAlsheikh0234@users.noreply.github.com>
2024-02-18 21:46:06 +0200

    Add files via upload

---
+++ EEPROM_driver.c
@@ -0,0 +1,27 @@
+/*
+ * EEPROM_driver.c
+ *
+ * Created: 12/3/2023 3:42:02 PM
+ *  Author: HaNdSyA---
+ */ 
+
+
+#include <avr/io.h>
+#include "macros.h"
+
+void EEPROM_write(unsigned short address,unsigned char data)
+{
+	EEARL=(char)address;      /*address register 16 bit*/
+	EEARH=(char)(address>>8);
+	EEDR=data;                /*data register*/
+	set_bit(EECR,EEMWE);    /*master write 1*/
+	set_bit(EECR,EEWE);     /*write 1*/
+	while (read_bit(EECR,EEWE)==1);    /*wait till write is 0*/
+}
+unsigned char EEPROM_read(unsigned short address)
+{
+	EEARL=(char)address;      
+	EEARH=(char)(address>>8);
+	set_bit(EECR,EERE);    /*read 1*/
+	return EEDR;          /*return data from the required address*/
+}
---
+++ EEPROM_driver.h
@@ -0,0 +1,17 @@
+/*
+ * EEPROM_driver.h
+ *
+ * Created: 12/4/2023 12:54:49 AM
+ *  Author: HaNdSyA---
+ */ 
+
+
+#ifndef EEPROM_DRIVER_H_
+#define EEPROM_DRIVER_H_
+void EEPROM_write(unsigned short address,unsigned char data);
+unsigned char EEPROM_read(unsigned short address);
+
+
+
+
+#endif /* EEPROM_DRIVER_H_ */
